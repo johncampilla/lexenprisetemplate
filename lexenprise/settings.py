8649ffc27c1069c0833475e234f38e3086a07ad5
John Sarandi Campilla <76666778+johncampilla@users.noreply.github.com>
2023-04-08 15:23:51 +0800

    Update Settings with Hybrid Connection

--- lexenprise/settings.py
+++ lexenprise/settings.py
@@ -76,10 +76,25 @@ WSGI_APPLICATION = 'lexenprise.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        'ENGINE': 'mssql',
+        'NAME': 'LexEnprise',
+        'USER': 'John',
+        'PASSWORD': '640515',
+        'HOST': '(LocalDb)\\MSSQLLocalDB',
+        'PORT': '',
     }
 }
 
